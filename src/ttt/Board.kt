5c89dc7764bad3a9f02025d1f6868d7bad596741
xXThermalXx <bjornfranke485@googlemail.com>
2018-02-11 02:03:49 +0100

    Update Board.kt

--- src/ttt/Board.kt
+++ src/ttt/Board.kt
@@ -2,7 +2,7 @@ package ttt
 
 class Board {
 
-    private var board = intArrayOf(0, 0, 0, 0, 1, 0, 0, 0, 0)
+    private var board = intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0)
     private val winConditions = arrayOf(intArrayOf(0, 1, 2), intArrayOf(3, 4, 5), intArrayOf(6, 7, 8), intArrayOf(0, 3, 6), intArrayOf(1, 4, 7), intArrayOf(2, 5, 8), intArrayOf(0, 4, 8), intArrayOf(2, 4, 6))
 
     fun list_moves(): IntArray {
